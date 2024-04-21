0816fc3c9f4decdca2655d59c84d536224833aae
Joshua Marble <jsmarble@gmail.com>
2024-04-21 10:08:03 -0500

    add api key validation to id route

--- src/index.ts
+++ src/index.ts
@@ -63,6 +63,35 @@ app.get("/hello/:name", async (c) => {
 });
 
 app.post("/id/:name/:uuid", async (c) => {
+  //first validate the api key
+  const apiKey = c.req.header("Authorization")?.replace("Bearer ", "");
+  if (!apiKey) {
+    return c.json(
+      {
+        error: "API key is required via Authorization header",
+      },
+      400
+    );
+  }
+
+  const v = await verifyKey(apiKey);
+  if (v.error) {
+    return c.json(
+      {
+        error: "Internal server error",
+      },
+      500
+    );
+  }
+  if (!v.result.valid) {
+    return c.json(
+      {
+        error: "Invalid API key",
+      },
+      401
+    );
+  }
+
   let uid = null;
   const uuid = guidSchema.safeParse(c.req.param("uuid"));
   if (uuid.success) {
